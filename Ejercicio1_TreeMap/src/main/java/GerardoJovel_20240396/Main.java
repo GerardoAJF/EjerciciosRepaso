08fd9f689dcdc2921f9269dd6af10ebd25f34b5e
unknown <Gerardo>
2025-05-30 15:44:55 -0600

    fourth commit

--- Ejercicio1_TreeMap/src/main/java/GerardoJovel_20240396/Main.java
+++ Ejercicio1_TreeMap/src/main/java/GerardoJovel_20240396/Main.java
@@ -1,8 +1,19 @@
 package GerardoJovel_20240396;
 
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        TreeMap<String, Integer> cities = new TreeMap<String, Integer>();
+
+        cities.put("San Salvador", 30);
+        cities.put("La Unión", 35);
+        cities.put("San Miguel", 33);
+        cities.put("Ahuachapán", 28);
+        cities.put("Chalatenango", 25);
+
+        System.out.println("Las ciudades son: ");
+        for (city : cities)
     }
 }
